78ecb693fa75f88eb50a42d93e0e9a5865869432
victorsmith <victor7smith@gmail.com>
2022-01-20 15:56:26 -0700

    delete button added

--- myscripts.js
+++ myscripts.js
@@ -1,21 +1,44 @@
 let myLibrary = []
 
 // testing
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
-const theHobbit2 = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
+const theHobbit = new Book(
+    myLibrary.length,
+    'The Hobbit',
+    'J.R.R Tolkien',
+    295,
+    false
+)
 myLibrary.push(theHobbit)
+
+const theHobbit2 = new Book(
+    myLibrary.length,
+    'The Hobbit',
+    'J.R.R Tolkien',
+    295,
+    false
+)
 myLibrary.push(theHobbit2)
 
-function Book(title, author, pages, read) {
-  this.name = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = () => {
-    let infoStr = `${title}, by ${author}\nPage Count: ${pages} pages\nStatus: ${read ? 'read' : 'not read'}`
-    
-    return infoStr
-  }
+
+
+// window.onchange = () => {
+// 	const main = document.querySelector('.main')
+// 	const bookCards = main.querySelectorAll('button')
+// 	console.log(bookCards)	
+// }
+
+
+
+function Book(index, title, author, pages, read) {   
+    this.index = index;
+    this.name = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = () => {
+        let infoStr = `${title}, by ${author}\nPage Count: ${pages} pages\nStatus: ${read ? 'read' : 'not read'}`
+        return infoStr
+    }     
 }
 
 function submitForm() {
@@ -25,7 +48,7 @@ function submitForm() {
     const pageNum = document.getElementById('pageNum').value;
     const finishedStatus = document.getElementById('read').checked;
 
-    const newBook = new Book (title, author, pageNum, finishedStatus);
+    const newBook = new Book (myLibrary.length, title, author, pageNum, finishedStatus);
     addBookToLibrary(newBook)
 
     document.getElementById('title').value = '';
@@ -40,26 +63,49 @@ function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-
 function displayLibraryContent() {
     const page = document.querySelector('.main');
-    page.innerHTML = '';
+  	page.innerHTML = '';
+  	
     for (book of myLibrary) {
-        const bookCard = document.createElement('div');
-        const newTitle = document.createElement('h2');
-        newTitle.textContent = `${book.info()}`;
-        newTitle.classList.add('book-card');
-        bookCard.appendChild(newTitle);
-        page.appendChild(bookCard);
-    }
-
+        const bookCard = `
+            <div class="book-card">
+                <h2>${book.index}</h2>
+                <h2>${book.name}</h2>
+                <h3>by ${book.author}</h3>
+                <h4>Pages: ${book.pages}</h4>
+                <h4>Read: ${book.read}</h4> 
+                <button class="delete-button" onclick="deleteCard(${book.index})">Delete</button>
+                <button class="toggle-button" onclick="toggleReadStatus(${book.index})">Toggle Status</button>
+            </div>`
+    	page.insertAdjacentHTML("beforeend", bookCard);
+  	}    
+    
+    
 }
 
+function deleteCard(targetIndex) {
+    console.log("Before: " + myLibrary)
+    myLibrary.forEach( (book) => {
+        console.log(book);
+        if (book.index == targetIndex) {
+            const index = myLibrary.indexOf(book)
+            if (index > -1) {
+                myLibrary.splice(index, 1)
+            }
+        }
+    })
+    console.log("After:" + myLibrary)
+    displayLibraryContent()
+}
 
-
-
+function toggleReadStatus(bookIndex) {
+    console.log(bookIndex);
+}
 
 window.onload = () => {
     displayLibraryContent()
 }
+
+
 
